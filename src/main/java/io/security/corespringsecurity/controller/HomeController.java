47d724171c0de42a23cdf73be32abe1151086988
ccj <dyn6zc6i5qrahdqj2rw6sd7cjovlgl6xwa6bvb5avtkrv2osrv4q>
2022-01-13 21:56:13 +0900

    학습을 위한 초기화

--- src/main/java/io/security/corespringsecurity/controller/HomeController.java
+++ src/main/java/io/security/corespringsecurity/controller/HomeController.java
@@ -1,16 +1,11 @@
 package io.security.corespringsecurity.controller;
 
 
-import io.security.corespringsecurity.security.authentication.services.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private UserDetailsServiceImpl userDetailsService;
 	
 	@GetMapping(value="/")
 	public String home() throws Exception {

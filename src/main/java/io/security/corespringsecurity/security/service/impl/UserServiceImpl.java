a2b6e3c714dadd6edcb02ed12eb66f9ca2306056
CJ0823 <changjun0823@gmail.com>
2022-01-17 11:50:34 +0900

    UserDetailsService를 구현하여 DB 연동 인증 처리

--- src/main/java/io/security/corespringsecurity/security/service/impl/UserServiceImpl.java
+++ src/main/java/io/security/corespringsecurity/security/service/impl/UserServiceImpl.java
@@ -1,10 +1,9 @@
-package io.security.corespringsecurity.service.impl;
+package io.security.corespringsecurity.security.service.impl;
 
 import io.security.corespringsecurity.domain.Account;
 import io.security.corespringsecurity.repository.UserRepository;
-import io.security.corespringsecurity.service.UserService;
+import io.security.corespringsecurity.security.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 

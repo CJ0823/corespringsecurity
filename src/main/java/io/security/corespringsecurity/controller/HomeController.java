2768904688bfac206767a2e541ef021222d73039
CJ0823 <changjun0823@gmail.com>
2022-01-18 10:52:39 +0900

    로그아웃 기능 추가

--- src/main/java/io/security/corespringsecurity/controller/HomeController.java
+++ src/main/java/io/security/corespringsecurity/controller/HomeController.java
@@ -1,20 +1,37 @@
 package io.security.corespringsecurity.controller;
 
 
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class HomeController {
-	
-	@GetMapping(value="/")
+
+	@GetMapping("/")
 	public String home() throws Exception {
 		return "home";
 	}
 
-	@GetMapping(value="/login")
+	@GetMapping("/login")
 	public String login() throws Exception {
 		return "login";
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request, HttpServletResponse response) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication != null) {
+			new SecurityContextLogoutHandler().logout(request, response, authentication); //로그아웃 필터를 통해서 처리함
+		}
+		return "redirect:/login";
 	}
 
 }

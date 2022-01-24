22f8d78ea184f79acc60784b62b23a9ee084c1b9
ccj <dyn6zc6i5qrahdqj2rw6sd7cjovlgl6xwa6bvb5avtkrv2osrv4q>
2022-01-25 07:05:57 +0900

    Ajax 요청 - CSRF 완료. html 파일의 submit, href=# 부분은 강의 대비 수정함

--- src/main/java/io/security/corespringsecurity/controller/HomeController.java
+++ src/main/java/io/security/corespringsecurity/controller/HomeController.java
@@ -2,17 +2,11 @@ package io.security.corespringsecurity.controller;
 
 
 import io.security.corespringsecurity.domain.Account;
-import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
@@ -22,25 +16,6 @@ public class HomeController {
 		return "home";
 	}
 
-	@GetMapping("/login")
-	public String login(@RequestParam(value = "error", required = false) String error,
-						@RequestParam(value = "exception", required = false) String exception, Model model) throws Exception {
-
-		model.addAttribute("error", error);
-		model.addAttribute("exception", exception);
-
-		return "login";
-	}
-
-	@GetMapping("/logout")
-	public String logout(HttpServletRequest request, HttpServletResponse response) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-		if (authentication != null) {
-			new SecurityContextLogoutHandler().logout(request, response, authentication); //로그아웃 필터를 통해서 처리함
-		}
-		return "redirect:/login";
-	}
 
 	@GetMapping("/denied")
 	public String accessDenied(String exception, Model model) {
